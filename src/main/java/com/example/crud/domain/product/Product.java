2e03ca2132836b9215f6a6217bcc73d74b41846f
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2024-02-27 14:19:51 -0300

    Dependencia faltando adicionada

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,23 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name="product")
+@Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of ="id")
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.UUID) private String id;
+    private String name;
+    private Number price_in_cents;
+}
