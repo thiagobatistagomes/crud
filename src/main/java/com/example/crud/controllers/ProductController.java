977c099a66631cb9f61f62fca7ec7cf8e54454b3
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2024-02-27 14:46:21 -0300

    criando método post

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,15 +1,31 @@
 package com.example.crud.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crud.domain.product.ProductRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
     @GetMapping
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok( "deu ok");
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid) {
+
     }
 }
+
+
--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Table(name="product")
