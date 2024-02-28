590525831bc47749e91730c1c2518ee6cfe7a182
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2024-02-28 17:01:48 -0300

    Métodos Put e Delete adicionados

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.example.crud.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +16,10 @@ import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -31,6 +38,27 @@ public class ProductController {
         Product newProduct = new Product(data);
         repository.save(newProduct);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
 
--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -1,6 +1,6 @@
 package com.example.crud.domain.product;
 
-import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
